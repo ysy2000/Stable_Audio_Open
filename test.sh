97b1d5bf154d95e9200823914df5a6bc9c956836
ysy2000 <suebook@naver.com>
2024-08-26 00:29:51 +0000

    runnable

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python3 train.py \
+--dataset-config /home/rxkmmm/Stable_Audio_Open/dataset.json \
+--model-config /home/rxkmmm/Stable_Audio_Open/model_config.json \
+--name stable_audio_open_finetune \
+--save-dir /home/rxkmmm/Stable_Audio_Open/checkpoints \
+--checkpoint-every 1000 \
+--precision 16-mixed \
+--seed 128 \
+--pretrained-ckpt-path /home/rxkmmm/Stable_Audio_Open/model.ckpt
+
+# --barch-size 64
+# --num-gpus 2
